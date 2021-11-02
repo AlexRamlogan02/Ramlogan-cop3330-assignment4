d15cdfa15e5e12b85947eaa815cdc9b3c363635f
AlexRamlogan02 <AlexRamlogan02@knights.ucf.edu>
2021-11-01 23:17:31 -0400

    hi

---
+++ src/main/java/ucf/assignments/ramlogancop3330assignment4/Test.java
@@ -0,0 +1,40 @@
+package ucf.assignments.ramlogancop3330assignment4;
+
+public class AppTest {
+
+    @Test
+    public static void create()
+    {
+        //makes sure that a List was actually created
+    }
+
+    @Test
+    private static void save()
+    {
+        //test if the file was actualy made, and if the list name is the same
+    }
+    @Test
+    private static void delete()
+    {
+        //makes sure that whatever is being deleted is no longer there
+    }
+
+    @Test
+    private static void look()
+    {
+       //goes through and makes sure that the method sorted the due dates properly
+    }
+
+    @Test
+    public void addItem()
+    {
+        //ensures that the item reads the same as the input
+    }
+
+    @Test
+    public void edit()
+    {
+        //makes sure the edited title equals the inputted title
+    }
+
+}
